d9d4ff0c1b34f6b1cbb6f7e838d1e8660c65f074
Rafael Caroni <87575645+kroonus2@users.noreply.github.com>
2024-03-08 18:59:38 -0300

    Update perceptron.py

--- Perceptron/perceptron.py
+++ Perceptron/perceptron.py
@@ -31,60 +31,37 @@ def andGate():
     input = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
     target = np.array([0, 0, 0, 1])
     w, b = treinamento(input, target)  # Chamada sem max_iter
-    print("AND Gate:")
-    for i in range(len(input)):
-        y = np.dot(input[i], w) + b >= 0
-        print("AND({}, {}) = {}".format(input[i][0], input[i][1], int(y)))
-    print(f'wFinal:: {w} - bFinal: {b}')
+    y = [int(np.dot(input[i], w) + b >= 0) for i in range(len(input))]
+    return y, w, b
 
 
 def orGate():
     input = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
     target = np.array([0, 1, 1, 1])
     w, b = treinamento(input, target)  # Chamada sem max_iter
-    print("OR Gate:")
-    for i in range(len(input)):
-        y = np.dot(input[i], w) + b >= 0
-        print("OR({}, {}) = {}".format(input[i][0], input[i][1], int(y)))
-    print(f'wFinal:: {w} - bFinal: {b}')
+    y = [int(np.dot(input[i], w) + b >= 0) for i in range(len(input))]
+    return y, w, b
 
 
 def nandGate():
     input = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
     target = np.array([1, 1, 1, 0])
     w, b = treinamento(input, target)  # Chamada sem max_iter
-    print("NAND Gate:")
-    for i in range(len(input)):
-        y = np.dot(input[i], w) + b >= 0
-        print("NAND({}, {}) = {}".format(input[i][0], input[i][1], int(y)))
-    print(f'wFinal:: {w} - bFinal: {b}')
+    y = [int(np.dot(input[i], w) + b >= 0) for i in range(len(input))]
+    return y, w, b
 
 
 def norGate():
     input = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
     target = np.array([1, 0, 0, 0])
     w, b = treinamento(input, target)  # Chamada sem max_iter
-    print("NOR Gate:")
-    for i in range(len(input)):
-        y = np.dot(input[i], w) + b >= 0
-        print("NOR({}, {}) = {}".format(input[i][0], input[i][1], int(y)))
-    print(f'wFinal:: {w} - bFinal: {b}')
+    y = [int(np.dot(input[i], w) + b >= 0) for i in range(len(input))]
+    return y, w, b
 
 
 def xorGate():
     input = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
     target = np.array([0, 1, 1, 0])  # Correct target values for XOR
     w, b = treinamento(input, target)  # Chamada sem max_iter
-    print("XOR Gate:")
-    for i in range(len(input)):
-        y = np.dot(input[i], w) + b >= 0
-        print("XOR({}, {}) = {}".format(input[i][0], input[i][1], int(y)))
-    print(f'wFinal:: {w} - bFinal: {b}')
-
-
-if __name__ == "__main__":
-    andGate()
-    orGate()
-    nandGate()
-    norGate()
-    xorGate()
+    y = [int(np.dot(input[i], w) + b >= 0) for i in range(len(input))]
+    return y, w, b
